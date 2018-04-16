b2708cc2e53505ee927d0bf9e53cb9109de83a08
Jose Dongil <jose.dongil@gmail.com>
2018-04-16 21:36:14 +0200

    [#1] Creación de componente vista AssetList

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/AssetList'
-import About from '@/views/AssetDetail'
+import AssetList from '@/views/AssetList'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    { 
+      path: '/', 
+      redirect: '/assets' 
+    },
+
     {
       path: '/assets',
       name: 'assets',
       component: AssetList
-    },
-    {
-      path: '/assets/:id',
-      name: 'asset',
-      component: AssetDetail
     }
   ]
 })
